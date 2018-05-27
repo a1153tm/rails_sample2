98b02c592a912be72d04925f2889d3a0442ab1ac
Taiji Miyabe <a1153tm@aiit.ac.jp>
2018-05-27 15:57:22 +0900

    Finish 10-2. Add friendly forwarding.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as @user
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {
@@ -19,9 +20,27 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
+  test "should redirect edit when not logged in" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: {
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: {
@@ -35,9 +54,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert name, @user.name
-    assert email, @user.email
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
   end
 
 end
-
